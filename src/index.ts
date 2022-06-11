5ee0b9c9aee7bb01ba52574ea08f1e18cdb041a1
Antun Nakić <antun.nakic@digital-abundance.hr>
2022-06-11 10:45:22 +0200

    Postavljen backend express server

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT_BACKEND;
+
+//middelwarei za dobiti body dio post zahtjeva, te za
+// potrpati parametre iz GET metode također na req objekt
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//ruteri
+//app.use('api/auth', authRouter);
+//app.use('api/user', userRouter);
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
